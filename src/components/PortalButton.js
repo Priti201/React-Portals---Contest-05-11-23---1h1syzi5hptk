029980457123e2e7a94c742430f8b4430ec33c3a
Priti Gupta <132835431+Priti201@users.noreply.github.com>
2023-11-05 10:37:53 +0530

    Update PortalButton.js

--- src/components/PortalButton.js
+++ src/components/PortalButton.js
@@ -4,7 +4,7 @@ import  ReactDOM  from 'react-dom';
 //complete this function using portals
 const PortalButton=()=>{
      return ReactDOM.createPortal(
-        <div>
+        <div id="portal-button">
             <button id="button" onClick={() => { props.buttonclick() }}>Click</button>
         </div>, document.getElementById("portal-button")
     )
