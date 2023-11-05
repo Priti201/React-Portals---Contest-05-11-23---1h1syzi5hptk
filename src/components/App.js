e282fca496d67d4a5ff401d4df1d027321fb5f85
Priti Gupta <132835431+Priti201@users.noreply.github.com>
2023-11-05 10:29:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,21 @@ import PortalTextArea from './PortalTextArea.js';
 //complete the function 
 const App = () => {
 
+const inpref = useRef(null);
+  const [val, setVal] = useState("");
+  const changeText = () => {
+     
+     setVal(inpref.current.value);
+     inpref.current.value="";
+  }
 
 
   return (
     
-    <div id="main">
-      <input id="input" value={} onChange={}></input><br/>
-      <PortalButton buttonclick={} /><br/>
-      <PortalTextArea value={}/>
+     <div id="main">
+      <input id="input" ref={inpref}></input><br />
+      <PortalButton buttonclick={changeText} /><br />
+      <PortalTextArea valu={val} />
     </div>
   )
 }
